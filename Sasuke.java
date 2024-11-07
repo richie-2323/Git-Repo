73fe3ecdd5cfe638eb3792ecffb40a2cfbbf021c
Homadeep-Jayadhar <homadeepjayadhar23@gmail.com>
2024-11-07 11:49:10 +0530

    Itacti

--- Sasuke.java
+++ Sasuke.java
@@ -1,5 +1,6 @@
-sausuke is villan
+Sasuke is villan
 
 But his Brother Itachi made him realise that he need to use his powers to save people who are in troble.
 
+Later he changed to a good ninja and saved the people.
 
